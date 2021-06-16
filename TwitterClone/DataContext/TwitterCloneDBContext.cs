416c188e72d0c15206c0c1d994ff213f58a1808c
SaiAnudeepK <anudeeph8@gmail.com>
2021-06-16 12:50:09 +0530

    Context and MIgration

---
+++ TwitterClone/DataContext/TwitterCloneDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TwitterClone.Models;
+
+namespace TwitterClone.DataContext
+{
+    public class TwitterCloneDBContext : DbContext
+    {
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Following> Following { get; set; }
+        public DbSet<Tweet> Tweet { get; set; }
+        public TwitterCloneDBContext(DbContextOptions<TwitterCloneDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ TwitterClone/Migrations/20210615074054_Database.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TwitterClone.Migrations
+{
+    public partial class Database : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Following",
+                columns: table => new
+                {
+                    User_Id = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Following_Id = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    UserID = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Joined = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Active = table.Column<byte>(type: "tinyint", maxLength: 1, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tweet",
+                columns: table => new
+                {
+                    TweetId = table.Column<int>(type: "int", maxLength: 10, nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User_ID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Message = table.Column<string>(type: "nvarchar(140)", maxLength: 140, nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tweet", x => x.TweetId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Following");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.DropTable(
+                name: "Tweet");
+        }
+    }
+}
