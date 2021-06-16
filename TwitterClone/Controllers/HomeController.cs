2ea7da5e2904974381a2b994ec16eadc0814478c
SaiAnudeepK <anudeeph8@gmail.com>
2021-06-16 12:49:36 +0530

    Controllers

--- TwitterClone/Controllers/HomeController.cs
+++ TwitterClone/Controllers/HomeController.cs
@@ -1,31 +1,81 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Web.Http;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using TwitterClone.Models;
+using TwitterClone.DataContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace TwitterClone.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly TwitterCloneDBContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, TwitterCloneDBContext context)
         {
             _logger = logger;
+            _context = context;
+
         }
 
-        public IActionResult Index()
+        //GET: Home/UserLogin
+        public IActionResult UserLogin()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+
+        //POST: Home/UserLogin
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UserLogin(Person userLogin)
+        {
+            var login = _context.Person.Where(a => a.UserID.Equals(userLogin.UserID)
+                                              && a.Password.Equals(userLogin.Password)).FirstOrDefault();
+            if (login != null)
+            {
+                Person session = _context.Person.SingleOrDefault(u => u.UserID == userLogin.UserID);
+                session.Active = 1;
+                HttpContext.Session.SetString("FullName", login.FullName);
+                HttpContext.Session.SetString("UserID", login.UserID);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                ViewBag.ErrMsg = "Invalid Credentials";
+                return View(userLogin);
+            }           
+        }
+
+        // GET: Home/Signup
+        public IActionResult Signup()
         {
             return View();
+        }
+
+        // POST: Home/Signup
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Signup([Bind("UserID, Password, FullName, Email, Joined")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                person.Active = 0;
+                person.Joined = DateTime.Now;
+                _context.Person.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("UserLogin");
+            }
+            return View(person);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ TwitterClone/Controllers/UserController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Web.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TwitterClone.Models;
+using TwitterClone.DataContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace TwitterClone.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly TwitterCloneDBContext _context;
+
+        public UserController(TwitterCloneDBContext context)
+        {
+            _context = context;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Logout()
+        {
+            Person session = _context.Person.SingleOrDefault(u => u.UserID == HttpContext.Session.GetString("UserID"));
+            session.Active = 0;
+            _context.Update(session);
+            _context.SaveChanges();
+            HttpContext.Session.Clear();
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            var users = _context.Person
+                .SingleOrDefault(m => m.UserID == HttpContext.Session.GetString("UserID"));
+
+            return View(users);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var users = await _context.Person.FindAsync(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+            return View(users);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("UserID, Password, FullName, Email")] Person users)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    users.Joined = users.Joined;
+                    _context.Update(users);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (users.UserID == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Profile");
+            }
+            return View(users);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
